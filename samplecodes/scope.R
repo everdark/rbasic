10150f782ccc8d5dd188fb265948e96da5c7651b
Kyle Chung <kylechun9@gmail.com>
2016-05-13 18:12:45 +0800

    two code samples

---
+++ samplecodes/scope.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+
+
+source("src.R")
+
+findVAR()
+
+VAR <- 1
+
+findVAR()
+
+main <- function() {
+    findVAR()
+}
+main()
---
+++ samplecodes/src.R
@@ -0,0 +1,5 @@
+
+
+
+findVAR <- function() exists("VAR")
+
